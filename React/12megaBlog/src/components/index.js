f7cad03ff0e8e1723fd28a01fb5660525612504f
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-04-10 20:48:50 +0600

    React

--- React/12megaBlog/src/components/index.js
+++ React/12megaBlog/src/components/index.js
@@ -4,6 +4,9 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import PostCard from "./PostCard"
 
 export {
     Header,
@@ -12,4 +15,7 @@ export {
     Logo,
     LogoutBtn,
     Button,
+    Input,
+    Select,
+    PostCard
 }
