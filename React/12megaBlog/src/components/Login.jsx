a053d71a29495e075077b834eee8771a79d31c18
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-04-13 11:48:18 +0600

    React

--- React/12megaBlog/src/components/Login.jsx
+++ React/12megaBlog/src/components/Login.jsx
@@ -36,13 +36,13 @@ function Login() {
             </div>
             <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
             <p className="mt-2 text-center text-base text-black/60">
-                        Don&apos;t have any account?&nbsp;
-                        <Link
-                            to="/signup"
-                            className="font-medium text-primary transition-all duration-200 hover:underline"
-                        >
-                            Sign Up
-                        </Link>
+                Don&apos;t have any account?&nbsp;
+                <Link
+                    to="/signup"
+                    className="font-medium text-primary transition-all duration-200 hover:underline"
+                >
+                    Sign Up
+                </Link>
             </p>
             {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
             <form onSubmit={handleSubmit(login)} className='mt-8'>
@@ -67,10 +67,9 @@ function Login() {
                         required: true,
                     })}
                     />
-                    <Button
-                    type="submit"
-                    className="w-full"
-                    >Sign in</Button>
+                    <Button type="submit" className="w-full">
+                        Sign in
+                    </Button>
                 </div>
             </form>
         </div>
