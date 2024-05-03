290b6f44037a60c545ba5d078ad38e2992de825c
Ashfaq684 <ashfaq.mostahid@gmail.com>
2024-05-03 23:41:56 +0600

    template

---
+++ Template/HTML-CSS-JS/Hospital management/auth.js
@@ -0,0 +1,15 @@
+const handleRegistration = (event) => {
+    event.preventDefault();
+    const username = getValue("username");
+    const first_name = getValue("first_name");
+    const last_name = getValue("last_name");
+    const email = getValue("email");
+    const password = getValue("password");
+    const confirm_password = getValue("confirm_password");
+}
+
+const getValue = (id) => {
+    const value = document.getElementById(id).value;
+    return value;
+}
+
